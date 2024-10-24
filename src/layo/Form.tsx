504cc805ffd7dd1196259c126a25cb55c1705cd5
somyagupta05 <115712949+somyagupta05@users.noreply.github.com>
2024-10-24 14:21:53 +0530

    Fix type error for children prop in Form component

--- src/layo/Form.tsx
+++ src/layo/Form.tsx
@@ -1,16 +1,40 @@
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-import Head from "next/head"; 
-export default function Form({children}){
-  return(
+// import Header from "@/components/Header"
+// import Footer from "@/components/Footer"
+// import Head from "next/head"; 
+// export default function Form({children}){
+//   return(
+//     <>
+//     <Head>
+//       <title>Blog</title>
+//     </Head>
+//     <Header></Header>
+//     <main>{children}</main>
+//     <br></br><br></br>
+//     <Footer></Footer>
+//     </>
+//   )
+// }
+
+import React, { ReactNode } from "react";
+import Footer from "@/components/Footer";
+import Head from "next/head";
+import Header from "@/components/Header";
+
+interface FormProps {
+  children: ReactNode;  // Define the type for children
+}
+
+export default function Form({ children }: FormProps) {
+  return (
     <>
-    <Head>
-      <title>Blog</title>
-    </Head>
-    <Header></Header>
-    <main>{children}</main>
-    <br></br><br></br>
-    <Footer></Footer>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <br />
+      <br />
+      <Footer />
     </>
-  )
+  );
 }
